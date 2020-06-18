1e6c6ba073fc974c642e40a3df1fc75263b82455
Renata Soikina <ren@soikin.ru>
2020-06-19 00:02:02 +0300

    Changed the Edit, Delete and Details views and actions of the controller.

--- TestCarWash/Controllers/ClientController.cs
+++ TestCarWash/Controllers/ClientController.cs
@@ -31,7 +31,6 @@ namespace TestCarWash.Controllers
             return View(client);
         }
 
-        // GET: Client/Create
         public ActionResult Create()
         {
             return View();
@@ -39,8 +38,7 @@ namespace TestCarWash.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Person, PhoneNumber")]
-            Client client)
+        public ActionResult Create([Bind(Include = "Person, PhoneNumber")] Client client)
         {
             try
             {
@@ -54,19 +52,18 @@ namespace TestCarWash.Controllers
             catch (DataException)
             {
                 //Log the error
-                ModelState.AddModelError("", "Невозможно сохранить. Попытайтесь позже, или обратитесь к администратору.");
+                ModelState.AddModelError("", "Невозможно сохранить. Попытайтесь позже или обратитесь к администратору.");
             }
             return View(client);
         }
 
-        // GET: Client/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Client client = db.Clients.Find(id);
+            var client = db.Clients.Find(id);
             if (client == null)
             {
                 return HttpNotFound();
@@ -74,30 +71,38 @@ namespace TestCarWash.Controllers
             return View(client);
         }
 
-        // POST: Client/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Person,PhoneNumber")] Client client)
+        public ActionResult Edit([Bind(Include = "Id, Person, PhoneNumber")] Client client)
         {
-            if (ModelState.IsValid)
+            try
             {
-                db.Entry(client).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    db.Entry(client).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException)
+            {
+                //Log the error
+                ModelState.AddModelError("", "Невозможно сохранить. Попытайтесь позже или обратитесь к администратору.");
             }
             return View(client);
         }
 
-        // GET: Client/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Delete(int? id, bool? saveChangesError=false)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Client client = db.Clients.Find(id);
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Не удалось удалить запись. Попытайтесь позже или обратитесь к администратору.";
+            }
+            var client = db.Clients.Find(id);
             if (client == null)
             {
                 return HttpNotFound();
@@ -105,14 +110,21 @@ namespace TestCarWash.Controllers
             return View(client);
         }
 
-        // POST: Client/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult Delete(int id)
         {
-            Client client = db.Clients.Find(id);
-            db.Clients.Remove(client);
-            db.SaveChanges();
+            try
+            {
+                var clientToDelete = new Client { Id = id };
+                db.Entry(clientToDelete).State = EntityState.Deleted;
+                db.SaveChanges();
+            }
+            catch (DataException)
+            {
+                //Log the error
+                return RedirectToAction("Delete", new {Id = id, saveChangesError = true});
+            }
             return RedirectToAction("Index");
         }
 
