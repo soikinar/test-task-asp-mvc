3088f628293c67f09c757dcaa9731e1b70e16eee
Renata Soikina <ren@soikin.ru>
2020-06-19 00:07:46 +0300

    Added the Service controller with actions and views from template.

---
+++ TestCarWash/Controllers/ServiceController.cs
@@ -0,0 +1,123 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using TestCarWash.Models;
+
+namespace TestCarWash.Controllers
+{
+    public class ServiceController : Controller
+    {
+        private CarWashContext db = new CarWashContext();
+
+        // GET: Service
+        public ActionResult Index()
+        {
+            return View(db.Services.ToList());
+        }
+
+        // GET: Service/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Service service = db.Services.Find(id);
+            if (service == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service);
+        }
+
+        // GET: Service/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Service/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,PricePerMinute")] Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Services.Add(service);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(service);
+        }
+
+        // GET: Service/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Service service = db.Services.Find(id);
+            if (service == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service);
+        }
+
+        // POST: Service/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,PricePerMinute")] Service service)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(service).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(service);
+        }
+
+        // GET: Service/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Service service = db.Services.Find(id);
+            if (service == null)
+            {
+                return HttpNotFound();
+            }
+            return View(service);
+        }
+
+        // POST: Service/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Service service = db.Services.Find(id);
+            db.Services.Remove(service);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
